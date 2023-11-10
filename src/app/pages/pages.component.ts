bab960caa77bff741f1fe0d6fc4261a06c9960f9
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-11-10 11:01:52 -0300

    add account-settings and change themes

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
 
+  ngOnInit(): void {
+    const url = localStorage.getItem('theme') || 'default-dark';
+
+    this.linkTheme?.setAttribute('href', url);
+  }
 }
