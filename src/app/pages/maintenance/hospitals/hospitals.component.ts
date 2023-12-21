5bab7bf846c5e213f1cb13b078ae2d0ef8420ab8
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-12-21 10:37:17 -0300

    feat: add loadHospitals

--- src/app/pages/maintenance/hospitals/hospitals.component.ts
+++ src/app/pages/maintenance/hospitals/hospitals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.model';
 import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
@@ -7,11 +8,22 @@ import { HospitalService } from 'src/app/services/hospital.service';
   styles: [],
 })
 export class HospitalsComponent implements OnInit {
+  public hospitals: Hospital[] = [];
+  public loading: boolean = true;
+
   constructor(private hospitalService: HospitalService) {}
 
   ngOnInit(): void {
+    this.loadHospitals();
+  }
+
+  loadHospitals() {
+    this.loading = true;
     this.hospitalService.loadHospitals().subscribe({
-      next: (hospitals) => console.log(hospitals),
+      next: (hospitals) => {
+        this.loading = false;
+        this.hospitals = hospitals;
+      },
       error: (error) => {
         console.log(error);
       },
