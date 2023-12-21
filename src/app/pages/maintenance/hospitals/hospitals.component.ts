4cad26361ac48eacd1d21468a7b831de4a231da0
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-12-21 09:30:24 -0300

    feat: add hospital service

---
+++ src/app/pages/maintenance/hospitals/hospitals.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HospitalService } from 'src/app/services/hospital.service';
+
+@Component({
+  selector: 'app-hospitals',
+  templateUrl: './hospitals.component.html',
+  styles: [],
+})
+export class HospitalsComponent implements OnInit {
+  constructor(private hospitalService: HospitalService) {}
+
+  ngOnInit(): void {
+    this.hospitalService.loadHospitals().subscribe({
+      next: (hospitals) => console.log(hospitals),
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => console.log('loadHospitals complete'),
+    });
+  }
+}
