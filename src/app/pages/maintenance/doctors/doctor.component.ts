40d3e8c53ef28b247a57a4227cd5fc6a26f18cfc
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-12-27 11:05:58 -0300

    feat: add loadhospitals

--- src/app/pages/maintenance/doctors/doctor.component.ts
+++ src/app/pages/maintenance/doctors/doctor.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-doctor',
   templateUrl: './doctor.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DoctorComponent {
+export class DoctorComponent implements OnInit {
+  public doctorForm: FormGroup;
+  public hospitals: Hospital[];
+  public selectedHospital: Hospital;
 
+  constructor(
+    private fb: FormBuilder,
+    private hospitalService: HospitalService
+  ) {}
+
+  ngOnInit(): void {
+    this.doctorForm = this.fb.group({
+      name: ['Sam', Validators.required],
+      hospital: ['', Validators.required],
+    });
+    this.loadHospitals();
+    this.doctorForm.get('hospital').valueChanges.subscribe({
+      next: (hospitalId) => {
+        this.selectedHospital = this.hospitals.find(
+          (h) => h._id === hospitalId
+        );
+      },
+    });
+  }
+
+  loadHospitals() {
+    this.hospitalService.loadHospitals().subscribe({
+      next: (hospitals: Hospital[]) => {
+        this.hospitals = hospitals;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => console.log('loadHospitals complete'),
+    });
+  }
+
+  saveDoctor() {
+    console.log(this.doctorForm.value);
+  }
 }
